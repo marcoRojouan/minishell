6b461f7bb80990e9c47706c54861960f25caf41f
Marco Rojouan <mrojouan@3D4.42angouleme.fr>
2026-04-13 11:23:38 +0200

    .h added with libs

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 11:00:52 by mrojouan          #+#    #+#             */
+/*   Updated: 2026/04/13 11:01:52 by mrojouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
