95617388628850e32804878b73cb5e1b20d6b9ed
Marco Rojouan <mrojouan@3D4.42angouleme.fr>
2026-05-12 11:33:32 +0200

    featuring: exec comm and files

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 15:20:33 by mrojouan          #+#    #+#             */
-/*   Updated: 2026/05/06 14:33:08 by mrojouan         ###   ########.fr       */
+/*   Updated: 2026/05/12 11:26:45 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,21 +78,6 @@ int	main(int ac, char **av, char **envp)
 			add_history(line);
 			if (!parsing(line, &shell))
 				ft_putstr_fd("minishell : parsing error\n", 2);
-			else if (shell.cmds[0]->args)
-			{
-				if (ft_strcmp(shell.cmds[0]->args[0], "pwd") == 0)
-					ft_pwd();
-				if (ft_strcmp(shell.cmds[0]->args[0], "env") == 0)
-					ft_env(shell.env);
-				if (ft_strcmp(shell.cmds[0]->args[0], "export") == 0)
-					ft_export(shell.cmds[0]->args, &shell.env);
-				if (ft_strcmp(shell.cmds[0]->args[0], "unset") == 0)
-					ft_unset(shell.cmds[0]->args, &shell.env);
-				if (ft_strcmp(shell.cmds[0]->args[0], "echo") == 0)
-					ft_echo(shell.cmds[0]->args);
-				if (ft_strcmp(shell.cmds[0]->args[0], "cd") == 0)
-					ft_cd(shell.cmds[0]->args, shell.env);
-			}
 		}
 		free(line);
 	}
